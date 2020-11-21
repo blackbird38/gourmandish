16838e588c424a6db0cd3edf074d88648d4cebe9
blackbird <blackbird38fr@gmail.com>
2020-11-21 22:05:42 +0100

    added routing, sidenav and login, signup components

--- gourmandish-client/src/app/app.module.ts
+++ gourmandish-client/src/app/app.module.ts
@@ -5,17 +5,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button'; 
+import { MatButtonModule } from '@angular/material/button';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatSidenavModule } from '@angular/material/sidenav'; 
+import { MatListModule } from '@angular/material/list';
+import { SignupComponent } from './signup/signup.component'; 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatButtonModule
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    AppRoutingModule
     
   ],
   providers: [],
