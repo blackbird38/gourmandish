d696f2ed080068851154453ea42bd1f6aad8e22c
blackbird <blackbird38fr@gmail.com>
2020-11-29 00:39:10 +0100

    added Mongo auth

--- gourmandish-server/README.md
+++ gourmandish-server/README.md
@@ -3,3 +3,19 @@
 `npm run dev` + check if it works `http://localhost:3000/ping`
 
 for the tests: `npm run test`
+
+---
+
+mongo shell (cmd: `mongo`):
+
+--- create the Mongo user that will be used for the connection to the MongoDB:---
+
+````> use gourmandishdb
+> db.createUser({
+    user: "root",
+    pwd: "root",
+    roles: [
+          { role: "readWrite", db: "gourmandishdb" }
+    ]
+  })```
+````
--- gourmandish-server/test/test_helper.js
+++ gourmandish-server/test/test_helper.js
@@ -1,11 +1,23 @@
 const mongoose = require('mongoose');
+const configDb = require("../config/database");
 
 mongoose.Promise = global.Promise;
 
+
+// TODO: check why the tests are using the main database and not the test database
 before((done) => {
-    mongoose.connect('mongodb://localhost/gourmandish_test');
+    const options = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: true,
+    };
+    //const mongoUri = 'mongodb://localhost/gourmandish_test';
+    const mongoUri = `mongodb://${configDb.mongo.user}:${configDb.mongo.pw}@${configDb.mongo.host}:${configDb.mongo.port}/` + `${configDb.mongo.dbTest}`;
+    console.log(mongoUri);
+    mongoose.connect(mongoUri, options);
     mongoose.connection
-        .once('open', () => { done(); })
+        .once('open', () => { console.log('Connected to MongoDB test'), done(); })
         .on('error', (error) => {
             console.warn('Warning', error);
         });
