e0fab9ff44e22063d8e5bff83986964afa706048
blackbird <blackbird38fr@gmail.com>
2020-11-29 11:48:28 +0100

    created menu and added a BehaviorSubject to keep the signIn state

--- gourmandish-client/src/app/app.component.ts
+++ gourmandish-client/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'gourmandish-client';
+  signedIn = false;
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.signedIn$.subscribe((signedIn) => {
+      this.signedIn = signedIn;
+    });
+  }
 }
--- gourmandish-client/src/app/auth/auth.service.ts
+++ gourmandish-client/src/app/auth/auth.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 interface UsernameAvailableResponse {
   available: boolean;
@@ -26,6 +28,7 @@ interface SignupResponse {
 })
 export class AuthService {
   apiUrl: string = 'http://localhost:3000/api/auth';
+  signedIn$ = new BehaviorSubject(false);
 
   constructor(private httpClient: HttpClient) {}
 
@@ -40,8 +43,15 @@ export class AuthService {
 
   signup(credentials: SignupCredentials): Observable<SignupResponse> {
     console.log(credentials);
-    return this.httpClient.post<SignupResponse>(`${this.apiUrl}/signup`, {
-      credentials,
-    });
+    return this.httpClient
+      .post<SignupResponse>(`${this.apiUrl}/signup`, {
+        credentials,
+      })
+      .pipe(
+        // if there  is an error at the signup, it won't reach here
+        tap(() => {
+          this.signedIn$.next(true);
+        })
+      );
   }
 }
