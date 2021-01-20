b855a130cb5944aad62d7e31a52c2cfd5fb44bee
blackbird38 <blackbird38fr@gmail.com>
2021-01-20 13:36:47 +0100

    added user-settings component

--- gourmandish-client/src/app/user/user.module.ts
+++ gourmandish-client/src/app/user/user.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { RecipesModule } from '../recipes/recipes.module';
+import { UserSettingsPageComponent } from './user-settings-page/user-settings-page.component';
 
 @NgModule({
-  declarations: [ProfilePageComponent],
+  declarations: [ProfilePageComponent, UserSettingsPageComponent],
   imports: [CommonModule, UserRoutingModule, RecipesModule],
 })
 export class UserModule {}
