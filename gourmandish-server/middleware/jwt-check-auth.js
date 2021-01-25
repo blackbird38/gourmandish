28b7705f9750d51633869c99f3c78a95989692b1
blackbird38 <blackbird38fr@gmail.com>
2021-01-25 17:04:10 +0100

    fixed bug

--- gourmandish-server/middleware/jwt-check-auth.js
+++ gourmandish-server/middleware/jwt-check-auth.js
@@ -2,12 +2,15 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   try {
-    console.log('[JWT-CHAECK-AUTH] hearder authorization' , req.headers.authorization);
+    console.log(
+      "[JWT-CHAECK-AUTH] hearder authorization",
+      req.headers.authorization
+    );
     const token = req.headers.authorization.split(" ")[1]; // "Bearer token" token is at idx 1
-    console.log('[JWT-CHAECK-AUTH] token' , token);
-    console.log('[JWT-CHAECK-AUTH] secret' , process.env.JWT_SECRET_KEY);
-    
-    const decodedToken = jwt.verify(JSON.parse(token), process.env.JWT_SECRET_KEY); // will use the user id to add it when a new recipe is added
+    console.log("[JWT-CHAECK-AUTH] token", token);
+    console.log("[JWT-CHAECK-AUTH] secret", process.env.JWT_SECRET_KEY);
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET_KEY); // will use the user id to add it when a new recipe is added
     console.log("decodedToken", decodedToken);
     printExpiringTokenDate(decodedToken.exp);
     // passing this to be used by the create a new recipe
@@ -21,7 +24,7 @@ module.exports = (req, res, next) => {
     // of valid token:
     next(); // execution continues
   } catch (error) {
-    console.error('[JWT-CHAECK-AUTH] Error', error);
+    console.error("[JWT-CHAECK-AUTH] Error", error);
     res.status(401).json({
       message: "You are not authenticated. Token error.",
     });
